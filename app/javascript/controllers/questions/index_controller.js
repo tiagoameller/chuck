cb9133436ef7092db713bd2612c81b67d13b38ef
Tiago Ameller <tiago.ameller@gmail.com>
2021-06-06 10:21:52 +0200

    new question template

--- app/javascript/controllers/questions/index_controller.js
+++ app/javascript/controllers/questions/index_controller.js
@@ -1,6 +1,22 @@
+import { cibTheMighty } from "@coreui/icons"
 import { QuestionsDatatable } from "datatables/questions"
 export default class QuestionsIndex extends ApplicationController {
+  static targets = ["radioByCategory", "radioByWord", "radioByRandom", "textByWord"]
+
   connect () {
     this.datatable = new QuestionsDatatable()
+  }
+
+  selectByWord () {
+    this.radioByWordTarget.checked = true
+  }
+
+  selectByCategory () {
+    this.radioByCategoryTarget.checked = true
+  }
+
+  reset () {
+    this.radioByCategoryTarget.checked = true
+    this.textByWordTarget.value = ""
   }
 }

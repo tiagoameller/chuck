722155720f1acae143fa9a454b59b0b107c85ea8
Tiago Ameller <tiago.ameller@gmail.com>
2021-06-06 11:26:41 +0200

    fill categories selector

--- app/javascript/controllers/questions/index_controller.js
+++ app/javascript/controllers/questions/index_controller.js
@@ -1,10 +1,48 @@
-import { cibTheMighty } from "@coreui/icons"
 import { QuestionsDatatable } from "datatables/questions"
+import CommonController from "controllers/shared/common_controller"
 export default class QuestionsIndex extends ApplicationController {
-  static targets = ["radioByCategory", "radioByWord", "radioByRandom", "textByWord"]
+  static targets = [
+    "radioByCategory",
+    "radioByWord",
+    "radioByRandom",
+    "textByWord",
+    "selectByCategory"
+  ]
 
   connect () {
     this.datatable = new QuestionsDatatable()
+    // making a request to early raises "self.postMessage is not a function", so do some delay
+    setTimeout(() => {
+      this.fillCategorySelect()
+    }, 500);
+  }
+
+  fillCategorySelect () {
+    self = this
+    const url = "https://api.chucknorris.io/jokes/categories"
+    const request = new XMLHttpRequest()
+    request.error = () => self.loadCategoriesError()
+    request.onload = (result) => {
+      const data = JSON.parse(request.responseText)
+      if (!Array.isArray(data)) {
+        self.loadCategoriesError()
+        return
+      }
+
+      this.selectByCategoryTarget.length = 0
+      let option
+      data.forEach((category) => {
+        option = document.createElement("option")
+        option.text = category
+        this.selectByCategoryTarget.add(option)
+      })
+    }
+    request.open("GET", url)
+    request.send()
+  }
+
+  loadCategoriesError () {
+    new CommonController().toast("Error", "Failed to retrieve categories", "error")
   }
 
   selectByWord () {
@@ -17,6 +55,8 @@ export default class QuestionsIndex extends ApplicationController {
 
   reset () {
     this.radioByCategoryTarget.checked = true
+    this.fillCategorySelect()
+    this.selectByCategoryTarget.selectedIndex = 0
     this.textByWordTarget.value = ""
   }
 }
